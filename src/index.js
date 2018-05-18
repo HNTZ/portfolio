8c93a2315788ab51ecc6883ede80cd2421ec80ec
Lucas Heintz <lucas.heintz@gmail.com>
2018-05-18 19:42:53 +0200

    Ajout images tech

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import printMe from './print.js';
 import './style.scss';
 import { cube } from './math.js';
 import menu from './menu.js';
-import Webs from './assets/img/web-s.svg'
-import Webm from './assets/img/web-m.svg'
-import Webl from './assets/img/web-l.svg'
+import Webs from './assets/img/web-s.svg';
+import Webm from './assets/img/web-m.svg';
+import Webl from './assets/img/web-l.svg';
+for (let i = 1; i < 13; i++) {
+    import i from '/assets/img/'+ i +'.png';
+}
 
 
 menu();
