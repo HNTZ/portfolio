b186910bc4e28a12eb84e7ba52c693bce5cfc856
Lucas Heintz <lucas.heintz@gmail.com>
2018-05-30 14:30:27 +0200

    pages projets + images

--- src/index.js
+++ src/index.js
@@ -7,36 +7,41 @@ import { TweenMax, TimelineLite, CSSPlugin } from "gsap/TweenMax";
 
 menu();
 
-let hoverEffects = (elements, hoverClass) => {
-    elements.forEach( (element) => {
-        element.addEventListener("mouseenter", () => {
-            element.classList.add(hoverClass);
-            console.log(element)
+let timeToTop = "0.6";
+
+function hoverpage() {
+
+    let hoverEffects = (elements, hoverClass) => {
+        elements.forEach( (element) => {
+            element.addEventListener("mouseenter", (e) => {
+                let slug = '#'+e.target.classList[1];
+                console.log(slug)
+                document.querySelector(slug).classList.add(hoverClass);
+                console.log(element)
+            })
+            element.addEventListener("mouseleave", (e) => {
+                let slug = '#'+e.target.classList[1];
+                document.querySelector(slug).classList.remove(hoverClass)
+            });
+            element.addEventListener("click", (e) => {
+                let slug = '#'+e.target.classList[1];
+                document.querySelector(slug).classList.add("clicked");
+                if (window.scrollY == 0) {
+                    timeToTop = 0;
+                }
+                setTimeout( function(){
+                    document.querySelector(slug).style.display = "none";
+                }, timeToTop * 1000)
+            })
         })
-        element.addEventListener("mouseleave", () => {
-            element.classList.remove(hoverClass)
-        })
-    })
+    }
+
+    let pages = document.querySelectorAll(".page-link");
+    hoverEffects(pages, 'hover');
 }
 
-let projets = document.querySelectorAll(".projets-item");
 
-let techs = document.querySelectorAll(".tech");
-
-let groupTechs = document.querySelectorAll(".group-tech");
-
-hoverEffects(projets, "hover");
-
-hoverEffects(techs, "hover");
-
-hoverEffects(groupTechs, "hover");
-
-document.addEventListener("click", function(e){
-    if (e.target.tagName === 'a') {
-        var href = e.target.href;
-        console.log(e.target)
-    }
-})
+hoverpage();
 
 var MovePage = Barba.BaseTransition.extend({
     start: function() {
@@ -52,8 +57,9 @@ var MovePage = Barba.BaseTransition.extend({
 
         console.log(obj)
 
-        TweenLite.to(obj, 0.4, {
+        TweenMax.to(obj, timeToTop,{
             y: 0,
+            ease: Power2.easeOut,
             onUpdate: function() {
                 if (obj.y === 0) {
                     deferred.resolve();
@@ -73,26 +79,31 @@ var MovePage = Barba.BaseTransition.extend({
         var _this = this;
 
         let versLaDroite = false
+        let depart = 100;
 
         if (this.getNewPageFile() == "home") {
-            versLaDroite = true;
+            versLaDroite = true
+        }
+        else if(this.newContainer.dataset.namespace == "projet" && this.getOldPageFile() == "home") {
+            depart = 50;
         }
 
         TweenMax.set(this.newContainer, {
             visibility: 'visible',
-            xPercent: versLaDroite ? -100 : 100,
+            xPercent: versLaDroite ? -depart : depart,
             position: "fixed",
             top: "150px",
             left: 0,
-            right: 0
+            right: 0,
+            ease: Power2.easeIn,
         });
 
-        TweenMax.to(this.oldContainer, 0.6, { xPercent: versLaDroite ? 100 : -100});
-        TweenMax.to(this.newContainer, 0.6, { xPercent: 0, onComplete: function() {
-                TweenMax.set(_this.newContainer, { clearProps: 'all' });
-                document.getElementsByTagName("body")[0].id = _this.getNewPageFile();
-                _this.done();
-            }});
+        TweenMax.to(this.oldContainer, 0.7, { xPercent: versLaDroite ? 100 : -100});
+        TweenMax.to(this.newContainer, 0.7, { xPercent: 0, onComplete: function() {
+            TweenMax.set(_this.newContainer, { clearProps: 'all' });
+            document.getElementsByTagName("body")[0].id = _this.getNewPageFile();
+            _this.done();
+        }});
     },
     getNewPageFile: function() {
         let pageFile = Barba.HistoryManager.currentStatus().url.split('/').pop().split('.')[0];
@@ -103,6 +114,15 @@ var MovePage = Barba.BaseTransition.extend({
             return pageFile
         }
         ;
+    },
+    getOldPageFile: function () {
+        let pageFile = Barba.HistoryManager.prevStatus().url.split('/').pop().split().shift()
+        if (pageFile == "") {
+            return "home"
+        }
+        else {
+            return pageFile
+        }
     }
 });
 
@@ -112,5 +132,28 @@ Barba.Pjax.getTransition = function() {
 
 Barba.Pjax.start();
 
+var Homepage = Barba.BaseView.extend({
+    namespace: 'homepage',
+    onEnterCompleted: function() {
 
-console.log(Barba.Pjax.getCurrentUrl())
+
+        hoverpage();
+    }
+});
+
+var Projet = Barba.BaseView.extend({
+    namespace: 'projet',
+    onEnterCompleted: function() {
+
+        let next = document.querySelector('a#next');
+        let prev = document.querySelector('a#prev');
+
+        hoverpage();
+    }
+})
+
+Homepage.init();
+
+Projet.init();
+
+
